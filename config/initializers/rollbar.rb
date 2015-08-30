0fd6f49032a8b73f40c0128d6a4b72c3c2aa2935
Matt Powell <fauxparse@gmail.com>
2015-08-30 21:58:27 +1200

    disable rollbar in development

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,6 +1,8 @@
 Rollbar.configure do |config|
   config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
 
+  config.enabled = false unless Rails.env.production?
+
   config.person_username_method = "name"
   config.person_email_method = "email"
 end

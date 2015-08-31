b10da013f92e1e6d65e0730d8db4871567f18057
Matt Powell <fauxparse@gmail.com>
2015-08-31 21:38:03 +1200

    fix pending payment issue

--- app/services/payments/process_external_payment.rb
+++ app/services/payments/process_external_payment.rb
@@ -1,34 +1,34 @@
 class ProcessExternalPayment
   include ActiveModel::Validations
-  
+
   attr_reader :params
-  
+
   validate :payment_is_pending
-  
+
   def initialize(params)
     @params = params
   end
-  
+
   def call
     return false if !valid?
-      
+
     if payment_was_successful?
       payment.approve!
     else
       payment.decline!
     end
   end
-  
+
   def payment
     @payment ||= Payment.find_by(token: params[:id])
   end
-  
+
   private
-  
+
   def payment_is_pending
-    errors.add(:base, "is not a pending payment!") if !payment.pending?
+    errors.add(:base, "is not a pending payment!") if !payment.try(:pending?)
   end
-  
+
   def payment_was_successful?
     params[:payment_status] == "Completed"
   end

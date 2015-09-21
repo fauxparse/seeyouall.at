7961b60b148ef508769e53efd6c2d271de9cd3c8
Matt Powell <matt.powell@powershop.co.nz>
2015-09-21 15:27:22 +1200

    fix activity creation with photo URLs

--- app/services/activities/create_activity.rb
+++ app/services/activities/create_activity.rb
@@ -1,14 +1,25 @@
 class CreateActivity
   def initialize(event, params)
     @event = event
-    @activity = @event.activities.build(params)
+    @params = params
   end
 
   def call
-    @activity.save
+    Activity.transaction do
+      photo_url = @params.delete(:photo_url)
+      @activity = @event.activities.build(@params)
+      add_photo(photo_url) if photo_url.present?
+      @activity.save
+    end
   end
 
   def activity
     ActivityPresenter.new(@activity)
+  end
+
+  private
+
+  def add_photo(photo_url)
+    @activity.activity_photos.build(photo: Photo.create(url: photo_url))
   end
 end
